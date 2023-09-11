5ccafcd9eef89e2da0177f5cab2845fcc37e6e3b
wolfb25 <fbalint0126@gmail.com>
2023-09-11 14:01:46 +0200

    nem lehet számot írni a számos textboxba

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,32 +15,34 @@ using System.Windows.Shapes;
 
 namespace Forditva2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
 
-        private void gomb_Click(object sender, RoutedEventArgs e) {
+		private void gomb_Click(object sender, RoutedEventArgs e) {
 
-        }
+		}
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
 
-        }
+		}
 
-        private void szo_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+		private void szo_SelectionChanged(object sender, SelectionChangedEventArgs e) {
 
-        }
+		}
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+		private void CheckBox_Checked(object sender, RoutedEventArgs e) {
 
-        }
+		}
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e) {
-
-        }
-    }
+		// számnak való
+		private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e) {
+			bool isNumeric = int.TryParse(szam.Text, out int val);
+			if (!isNumeric) szam.Text = new string(szam.Text.Take(szam.Text.Length - 1).ToArray());
+		}
+	}
 }
